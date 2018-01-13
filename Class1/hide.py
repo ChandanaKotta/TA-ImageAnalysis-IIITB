e44abbb09703f561537321b0468faf26cd37b9ab
ChandanaKotta <chandanakotta@gmail.com>
2018-01-13 22:50:52 +0530

    comments added

--- Class1/hide.py
+++ Class1/hide.py
@@ -1,14 +1,20 @@
 # play with bits
+# Extending the previous ideas about the most significant bits in pixels in the context of capturing information.
+# This property can be exploited to fuse images. So may be used for encryption.
+# In the following example, we will try to fuse Mr.Beans image over an image of Mona Lisa.
+
 import cv2
 import numpy as np
 
 mona_lisa = cv2.imread("mona.jpg")
-
 mr_bean = cv2.imread("mr_bean.jpg")
+
+# Re-sizing mr.beans image to have the same dimensions as that of Mona Lisa.
 mr_bean = mr_bean[:,100:700,:]
 mr_bean = cv2.resize(mr_bean,(604,900))
 print mona_lisa.shape
 print mr_bean.shape
+
 height, width, channels = mona_lisa.shape
 
 number_of_bits = 2
